5568dc3f83463403508ef73ff62db5483fbce240
Семён Крекотун <samkrekotyn@gmail.com>
2022-04-21 00:16:13 +0300

    добавляет стили products

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,12 @@ let navMain = document.querySelector('.main-header');
 let navToggle = document.querySelector('.main-header__toggle');
 
 navMain.classList.remove('main-header--nojs');
+navMain.classList.add('main-header--menu_closed');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-header--menu_closed')) {
     navMain.classList.remove('main-header--menu_closed');
-    navMain.classList.add('main-header--opened');
+    navMain.classList.add('main-header--menu_opened');
   } else {
     navMain.classList.add('main-header--menu_closed');
     navMain.classList.remove('main-header--menu_opened');
