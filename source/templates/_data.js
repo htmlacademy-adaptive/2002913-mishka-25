29e005c16e23a0a96b41eceeda1dac78bf532f95
SemenK <samkrekotyn@gmail.com>
2022-05-05 10:44:05 +0300

    добавляет шаблонизвтор картинок

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -12,5 +12,52 @@ export default {
       "path": "form.html",
       "name": "Вязание на заказ"
     }
-  ]
+  ],
+  "viewports": {
+    "desktop": 1150,
+    "tablet": 768,
+    "mobile": null
+  },
+  "images": {
+    "triple-set": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 315,
+      "maxdppx": 2,
+      "alt": "Набор из трёх вязанных корзинок.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/bunny": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязанный зайчик-попрыгайчик голубого цвета.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/laundry-basket": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязанная корзина наполненная полотенцами.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/toy-basket": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязанная корзина наполненная мягкими игрушками.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/video": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 150,
+      "maxdppx": 2,
+      "alt": "Тут должно быть интересное видео.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+  }
 }
