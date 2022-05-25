dd4dcd3bc1443627f245c9d3cdd2ffa275d0d55d
SemenK <samkrekotyn@gmail.com>
2022-05-25 12:11:02 +0300

    fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
-let navMain = document.querySelector('.main-header');
+let navMain = document.querySelector('.main-header__wrapper');
 let navToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-header--nojs');
-navMain.classList.add('main-header--menu_closed');
+navMain.classList.remove('main-header__wrapper--nojs');
+navMain.classList.add('main-header__wrapper--menu_closed');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header--menu_closed')) {
-    navMain.classList.remove('main-header--menu_closed');
-    navMain.classList.add('main-header--menu_opened');
+  if (navMain.classList.contains('main-header__wrapper--menu_closed')) {
+    navMain.classList.remove('main-header__wrapper--menu_closed');
+    navMain.classList.add('main-header__wrapper--menu_opened');
   } else {
-    navMain.classList.add('main-header--menu_closed');
-    navMain.classList.remove('main-header--menu_opened');
+    navMain.classList.add('main-header__wrapper--menu_closed');
+    navMain.classList.remove('main-header__wrapper--menu_opened');
   }
 });
